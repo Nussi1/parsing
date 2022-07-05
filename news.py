63e9d8d29a1930ad29491554a72ebec26c4e110a
Nussi1 <nuraika.obozkanova@gmail.com>
2022-07-05 17:04:01 +0600

    added docstring

--- news.py
+++ news.py
@@ -6,6 +6,9 @@ import json
 limit = int(input('limit of news: '))
 URL = (f"http://newsline.kg/getNews.php?limit={limit}&last_dt={datetime.now()}")
 
+'''
+etot function beret is URL dannye i zapisyvaet v new file s formate json, s ukazaniem tekushei daty (format daty dd/mm/yy) s otstupom 4 shaga
+'''
 
 def get_news():
     t_date = datetime.now().strftime('%d_%m_%Y')
